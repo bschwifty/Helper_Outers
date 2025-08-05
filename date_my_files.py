7923f53fc72dd71034e5af7148b1c1baee9ab8d0
Voltron <barrettschake@Barretts-MacBook-Pro.local>
2025-08-05 10:25:37 +0100

    started date files script

---
+++ date_my_files.py
@@ -0,0 +1,11 @@
+# This script is supposed to help out by renaming all the files in a folder to have their creation
+# date right at the beginning, e.g., "file.txt" becomes "20250731_file.txt".
+# Great way to stay organized!
+
+from datetime import datetime
+
+print("Welcome to date_my_files. I will now prepend each file in this directory with the date\n" \
+" it was created. Press any key to proceed, or ctrl+c to cancel : )")
+
+
+
